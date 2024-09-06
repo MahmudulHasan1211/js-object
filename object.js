f1c3d6ab30923b4f1db5f8bf3cc71313a7d07add
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-06 22:31:28 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,21 @@
+// object in js 
+const person = {
+    name: 'mahmudul',
+    age: 23,
+    profession: 'student',
+    No: 'No status'
+};
+
+console.log(person.name); // Output: 'mahmudul'
+//  dot notation 
+const boyos =person.age
+console.log(boyos)
+
+
+// another way 
+let car ={
+    accelerate: function(){
+        console.log('car speed-up');
+    }
+}
+car.accelerate();// output: car speed-up
